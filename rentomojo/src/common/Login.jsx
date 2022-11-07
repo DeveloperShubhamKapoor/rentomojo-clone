c5c72aa7b8016944bc1fae075875eb56c7637da2
shubham <kapoorshubham483@gmail.com>
2022-11-07 19:41:54 +0530

    added: signup login logic

--- rentomojo/src/common/Login.jsx
+++ rentomojo/src/common/Login.jsx
@@ -1,13 +1,47 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import styles from '../styles/login_signup.module.css'
 import Navbar from './Navbar'
 import logo from '../images/rentomojo_cat_login.jpeg'
+import { useNavigate } from 'react-router-dom'
 const initInfo ={
   email:"",
   password:""
 }
+const initLogin={
+  msg:"",
+  token:"",
+  login:false
+}
 export const Login=()=>{
   const [userLogin,setUserLogin] = useState(initInfo)
+  const [isLoginSuccess,setIsLoginSuccess] = useState(initLogin)
+  const navigate = useNavigate()
+  const handleChange=(e)=>{
+    const {name,value} = e.target
+    setUserLogin({
+      ...userLogin,
+      [name]:value
+    })
+  }
+  const handleLogin=()=>{
+    fetch("http://localhost:5500/login",{
+      method:"POST",
+      headers:{
+        "Content-Type":"application/json"
+      },
+      body:JSON.stringify(userLogin)
+    })
+    .then((res)=> res.json())
+    .then((data)=>setIsLoginSuccess(data))
+  }
+  useEffect(()=>{
+    if(isLoginSuccess.login){
+      localStorage.setItem("token_rento_mojo",JSON.stringify(isLoginSuccess.token))
+      localStorage.setItem("userEmail",JSON.stringify(userLogin.email))
+      navigate("/")
+    }
+
+  },[isLoginSuccess])
   return (
     <div>
 <Navbar/>
@@ -17,11 +51,11 @@ export const Login=()=>{
         </div>
         <div className={styles.info_box}>
           <div className={styles.info_input_box}>
-            <input type="text" placeholder='Enter Email' />
+            <input type="text" placeholder='Enter Email' onChange={handleChange} name='email' />
             <br />
-            <input type="password" placeholder='Enter Password' />   
+            <input type="password" placeholder='Enter Password' onChange={handleChange} name='password' />   
             <br />
-            <button>Login</button>
+            <button onClick={handleLogin}>Login</button>
           </div>
         </div>
       </div>
--- rentomojo/src/common/Signup.jsx
+++ rentomojo/src/common/Signup.jsx
@@ -1,5 +1,5 @@
 
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import styles from '../styles/login_signup.module.css'
 import Navbar from './Navbar'
 import logo from '../images/rentomojo_cat_login.jpeg'
@@ -9,9 +9,40 @@ const initInfo={
   email:"",
   password:""
 }
+const initData={
+  msg:"",
+  signupSuccess:false
+}
 export const Signup=()=>{
   const [userSignup,setUserSignup] = useState(initInfo)
+  const [isSignupSuccess,setIsSignupSuccess] = useState(initData)
   const navigate=useNavigate()
+
+  const handleOnChange=(e)=>{
+    const{name,value} = e.target
+    setUserSignup({
+      ...userSignup,
+      [name]:value
+    })
+  }
+
+  const handleOnClick=()=>{
+    fetch("http://localhost:5500/signup",{
+      method:"POST",
+      headers:{
+        "Content-Type":"application/json"
+      },
+      body:JSON.stringify(userSignup)
+    })
+    .then((res)=> res.json())
+    .then((data)=>setIsSignupSuccess(data))
+  }
+  useEffect(()=>{
+    if(isSignupSuccess.signupSuccess== true){
+      navigate("/login")
+    }
+    
+  })
   return (
     <div>
       <Navbar/>
@@ -21,11 +52,11 @@ export const Signup=()=>{
         </div>
         <div className={styles.info_box}>
           <div className={styles.info_input_box}>
-            <input type="text" placeholder='Enter Email' />
+            <input type="text" placeholder='Enter Email' name="email"  onChange={handleOnChange}/>
             <br />
-            <input type="password" placeholder='Enter Password' />   
+            <input type="password" placeholder='Enter Password' name="password" onChange={handleOnChange} />   
             <br />
-            <button>Register</button>
+            <button onClick={handleOnClick}>Register</button>
             <div className={styles.login_info_box}>
                 <p>Already Registered? Login Instead</p>
                 <button onClick={()=>{navigate("/login")}}>Login</button>
